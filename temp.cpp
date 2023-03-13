c8453b892e432a1af5e1685a29819b7e65c0af4e
jay nakarani <jaynakarani5@gmail.com>
2023-03-13 16:32:17 +0530

    temp second commit

--- temp.cpp
+++ temp.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n,m, sum = 0;
+    int n,m, sum = 0,minus=0;
 
     cout << "Enter a positive integer 1: ";
     cin >> n;
@@ -10,7 +10,7 @@ int main() {
     cin >> m;
 
     sum=m+n;
-
-    cout << "Sum = " << sum;
+    minus=m-n;
+    cout << "Sum = " << sum<< "minus = "<<minus;
     return 0;
 }
