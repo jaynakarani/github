d66006af331169a8912d7a10cf56c3fd3e9980c3
jay nakarani <jaynakarani5@gmail.com>
2023-03-13 16:25:43 +0530

    first commit

---
+++ temp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m, sum = 0;
+
+    cout << "Enter a positive integer 1: ";
+    cin >> n;
+    cout << "Enter a positive integer 2: ";
+    cin >> m;
+
+    sum=m+n;
+
+    cout << "Sum = " << sum;
+    return 0;
+}
